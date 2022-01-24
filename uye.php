91bd0207d20297ed95fa88aa53cb6f977c750fff
kubrademetgul <kubra_demetgul@outlook.com>
2022-01-25 01:49:51 +0300

    Initial commit

---
+++ uye.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href="style.css">
+    <title>Hastane Otomasyonu</title>
+</head>
+<body>
+    <header>
+        <h2>Hastane Otomasyonuna Hoş Geldiniz</h2>
+    </header>
+
+    <div class="tableOuter">
+        <h1>Giriş Yap</h1>
+        <form action="islem.php" method="post">
+            <div class="user">
+                <input type="text" name="kullanici_adsoyad" placeholder="Adınız ve Soyadınız">
+             </div>
+            <div class="user">
+                <input type="text" name="kullanici_tc" placeholder="Tc Kimlik No">
+             </div>
+             <div class="pass"> 
+                <input type="password" name="kullanici_password" placeholder="Şifre">
+             </div>
+             <button type="submit" class="sub" id="giris" name="kullanici_kaydet">Üye Ol</button>
+             <br>
+        </form>
+        <a href="index.php"><button type="submit" class="sub" id="uye">Geri Çık</button></a>
+    </div>
+</body>
+</html>
